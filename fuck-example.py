be6df8b9de1b3a3d28c1a7cc024a3b5a33188acb
Pizero <PizeroHelp@outlook.com>
2022-07-01 23:08:26 +0800

    add fuck-example.py的说明

--- fuck-example.py
+++ fuck-example.py
@@ -1,11 +1,22 @@
 import time
 import GoCqhttpApi
 
-uid = ''
-gid = ''
-msgs = [' ',' ',' ',' ']
+'''
+uid是发给谁的uid
+gid是用来规定发哪个群里，如果把gid删除，则为使用私信发送，没有好友就直接报错
+'''
 
-for msg in msgs:
-    message = msg
+uid = '1060432245'
+gid = '1004741240'
+
+'''
+下面 GoCqhttpApi.sendmsg 中，第一格为发送的信息，第二格为uid，第三格为gid
+time.sleep(1) 是在你怕被封号的时候加上，如果你不怕被封，随便用，不加也行
+'''
+
+GoCqhttpApi.sendmsg('你有没有对可乐兔三番五次的禁言感到烦躁，你有没有对在滥用权力感到愤怒，你有没有听到被禁言者在悲鸣',uid,gid)
+
+for msg in range(100):
+    message = '可乐兔，我日你先人'
     GoCqhttpApi.sendmsg(message,uid,gid)
-    time.sleep(1)
+    # time.sleep(1)
