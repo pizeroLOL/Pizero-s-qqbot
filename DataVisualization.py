b4337158851032a3c1eb76a901ef45cdfb1352a4
pizero <pizerohelp@outlook.com>
2022-07-27 22:30:05 +0800

    格式化并添加功能 server，将可视化变为函数，在server中添加了该函数触发

--- DataVisualization.py
+++ DataVisualization.py
@@ -1,14 +1,16 @@
 
 '''
-编写参考 
+编写参考
 https://www.cnblogs.com/xiaoshun-mjj/p/14516964.html
 https://blog.csdn.net/liuyingying0418/article/details/100126348
 '''
-from time import sleep
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 import sys
+import time
+import GoCqhttpApi
 
 # 常用参数导入
 # csv_path = sys.argv[1]
@@ -19,55 +21,128 @@ plt.rcParams['axes.unicode_minus'] = False
 # 画图的样式风格设置为：ggplot
 plt.style.use('ggplot')
 
-# 读取 csv
-# df = pd.read_csv(csv_path)
-with open('D:/编写中/cqhttp/apps/msglog/gid-1004741240/2022-07.csv', 'r', encoding='utf-8') as csv:
-    df = pd.read_csv(csv)
+# # 读取 csv
+# df = None
 
-"""时间格式转换"""
-# 获取时间
-df['time'] = pd.to_datetime(df['time'], format='%Y-%m-%d %H:%M:%S')
-# 月
 
-'''消息发送top 排名'''
-uid_step = df['uid'].value_counts()
-uid_step.columns = ['左uid，右次数']
-uid_top = uid_step.reset_index()
-uid_top.index = range(len(uid_top))
-uid_top.index=uid_top.index+1
-uid_top.columns = ['uid','次数']
-print('单位时间发送消息top20')
-print(uid_top.head(20))
+class PermissionError(BaseException):
+    '''你他喵要么每天好地址，要么没填好gid'''
+    ...
 
-'''单位时间发送消息分布'''
-plt.figure(figsize=(5, 3))
-plt.subplot(1, 1, 1)
-uid_step.plot.hist(bins=30, title='发送消息分布')
-plt.savefig('发送消息分布')
 
-'''活跃时间散点图'''
-df['hour'] = df['time'].dt.hour
-df['minute'] = df['time'].dt.minute
-df['second'] = df['time'].dt.second
-df['float_hours'] = df['hour'] + df['minute']/60
-msg_presecend = df['float_hours'].value_counts()
-msg_presecend = msg_presecend.reset_index()
-msg_presecend.columns = ['时间-小时与分钟', '消息数']
-msg_presecend.plot.scatter(x='消息数', y='时间-小时与分钟')
-plt.savefig('活跃时间散点图')
-# print(msg_presecend)
-# plt.show()
+def read_csv(uid: str | int, gid: str | int, msglog_path: str, types: str):
+    '''读取 csv'''
+    global df, uid_step, msglog_paths
+    path = msglog_path + '/gid-' + \
+        str(gid)+'/'+time.strftime("%Y-%m", time.localtime())+'.csv'
+    try:
+        with open(path, 'r', encoding='utf-8') as csv:
+            df = pd.read_csv(csv)
+    except PermissionError as e:
+        print('无指定群号')
+        # GoCqhttpApi.sendmsg('无该群聊记录', uid, gid)
+    """时间格式转换"""
+    df['time'] = pd.to_datetime(df['time'], format='%Y-%m-%d %H:%M:%S')
+    # print(df)
+    return table_conversion(uid, gid,  types, msglog_path)
 
-'''每小时在线分布'''
-msg_prehours = df['hour'].value_counts()
-msg_prehours = msg_prehours.reset_index()
-msg_prehours.columns = ['小时', '消息数']
-msg_prehours = msg_prehours.sort_values(by = ['小时', '消息数'])
-msg_prehours = msg_prehours.reset_index(drop=True)
-msg_prehours.plot(x='小时', y='消息数')
-plt.savefig('每小时在线分布')
-# print(msg_prehours)
-# plt.show()
+
+def table_conversion(uid: str, gid: str, types: str, msglog_path='./msglog'):
+    df['hour'] = df['time'].dt.hour
+    df['minute'] = df['time'].dt.minute
+    df['second'] = df['time'].dt.second
+    df['float_hours'] = df['hour'] + df['minute']/60
+    match types:
+        case 'uid_step' | 'uid_top':
+            uid_step = df['uid'].value_counts()
+            uid_step.columns = ['左uidX，右次数']
+            if types == 'uid_top':
+                uid_top = uid_step.reset_index()
+                uid_top.index = range(len(uid_top))
+                uid_top.index = uid_top.index+1
+                uid_top.columns = ['uid', '次数']
+                return uid_top
+            return uid_step
+        case 'msg_presecend':
+            msg_presecend = df['float_hours'].value_counts()
+            msg_presecend = msg_presecend.reset_index()
+            msg_presecend.columns = ['时间-小时与分钟', '消息数']
+            return msg_presecend
+        case 'msg_prehours':
+            msg_prehours = df['hour'].value_counts()
+            msg_prehours = msg_prehours.reset_index()
+            msg_prehours.columns = ['小时', '消息数']
+            msg_prehours = msg_prehours.sort_values(by=['小时', '消息数'])
+            msg_prehours = msg_prehours.reset_index(drop=True)
+            return msg_prehours
+
+
+def monthly_sendmsg_top20(uid: str | int, gid: str | int, msglog_path='./msglog'):
+    '''消息发送top20'''
+    uid_top = read_csv(uid, gid, msglog_path, 'uid_top')
+    tmp_fm = uid_top.head(20)
+    tmp_fm2list_1 = list(tmp_fm['uid'])
+    tmp_fm2list_2 = list(tmp_fm['次数'])
+    msg = '本月消息发送top20%0A QQ号        uid%0A'
+    if len(tmp_fm2list_1) != len(tmp_fm2list_2):
+        print('长度不符，请检查代码')
+        GoCqhttpApi.sendmsg('长度不符，请检查代码', str(uid), str(gid))
+    for x in range(len(tmp_fm2list_1)):
+        msg = msg + str(tmp_fm2list_1[x]) + '  ' + str(tmp_fm2list_2[x])+'%0A'
+    GoCqhttpApi.sendmsg(msg, str(uid), str(gid))
+    # print(msg)
+
+
+def nmsm(uid: str | int, gid: str | int, msglog_path='./msglog'):
+    '''当月单位消息数发送成员数'''
+    return the_number_of_units_in_the_month_of_the_month_sent_the_number_of_members(uid, gid, msglog_path)
+
+
+def the_number_of_units_in_the_month_of_the_month_sent_the_number_of_members(uid: str | int, gid: str | int, msglog_path='./msglog'):
+    '''当月单位消息数发送成员数'''
+    uid_step = read_csv(uid, gid, msglog_path, 'uid_step')
+    plt.figure(figsize=(5, 3))
+    plt.subplot(1, 1, 1)
+    uid_step.plot.hist(bins=30, title='发送消息分布')
+    plt.savefig('发送消息分布')
+    paths = 'file:///' + \
+        os.path.split(os.path.realpath(sys.argv[0]))[0] + '\发送消息分布.png'
+    # print(paths)
+    GoCqhttpApi.image(uid, gid, paths)
+
+
+def atsm(uid: str | int, gid: str | int, msglog_path='./msglog'):
+    '''活跃时间散点图'''
+    return active_time_scatter_map(uid, gid, msglog_path)
+
+
+def active_time_scatter_map(uid: str | int, gid: str | int, msglog_path='./msglog'):
+    '''活跃时间散点图'''
+    msg_presecend = read_csv(uid, gid, msglog_path, 'msg_presecend')
+    msg_presecend.plot.scatter(x='消息数', y='时间-小时与分钟')
+    plt.savefig('活跃时间散点图')
+    paths = 'file:///' + \
+        os.path.split(os.path.realpath(sys.argv[0]))[0] + '\活跃时间散点图.png'
+    # print(paths)
+    GoCqhttpApi.image(uid, gid, paths)
+    # plt.show()`
+
+
+def sinehtm(uid: str | int, gid: str | int, msglog_path='./msglog'):
+    return send_information_number_every_hour_that_month(uid, gid, msglog_path)
+
+
+def send_information_number_every_hour_that_month(uid: str | int, gid: str | int, msglog_path='./msglog'):
+    '''当月每小时发送信息条数折线图'''
+    msg_prehours = read_csv(uid, gid,  msglog_path, 'msg_prehours')
+    # print(msg_prehours)
+    msg_prehours.plot(x='小时', y='消息数')
+    plt.savefig('每小时在线分布')
+    paths = 'file:///' + \
+        os.path.split(os.path.realpath(sys.argv[0]))[0] + '\每小时在线分布.png'
+    # print(paths)
+    GoCqhttpApi.image(uid, gid, paths)
+    # plt.show()
 
 # '''最晚消息'''
 # lastest_msg=df
@@ -77,3 +152,10 @@ plt.savefig('每小时在线分布')
 #     lastest_msg.index=df['minute']
 
 # print(hour_tmp)
+
+
+# if __name__ == '__main__':
+#     monthly_sendmsg_top20(2774737215, 1004741240)
+#     nmsm(2774737215, 1004741240)
+#     atsm(2774737215, 1004741240)
+#     sinehtm(2774737215, 1004741240)
--- GoCqhttpApi.py
+++ GoCqhttpApi.py
@@ -1,4 +1,6 @@
-import requests,configparser,time
+import requests
+import configparser
+import time
 
 config = configparser.ConfigParser()
 config.read('app-config.cfg')
@@ -10,36 +12,44 @@ post = str(uid_topsecret['request-post'])
 global_uid = None
 msg_step = 0
 
-def sendmsg(msg,uid,gid = None):
+
+def sendmsg(msg: str, uid: str | int, gid=None):
     '''发送消息，当gid为空时发送私聊消息'''
-    '''刹车'''
-    if time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) != diff_time and gid == global_uid:
-        diff_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        msg_step = msg_step + 1
-    else:
-        msg_step = 0
-        global_uid = uid
-    if msg_step == 2:
-        time.sleep(1)
-        return sendmsg('触发频率过高请再发一次')
+    '''初始化'''
+    uid = str(uid)
+    gid = str(gid)
+    # '''刹车'''
+    # if time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) != diff_time and gid == global_uid:
+    #     diff_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    #     msg_step = msg_step + 1
+    # else:
+    #     msg_step = 0
+    #     global_uid = uid
+    # if msg_step == 2:
+    #     time.sleep(1)
+    #     return sendmsg('触发频率过高请再发一次')
     '''正常发送流程'''
     if gid != None:
-        requests.get('http://{0}:{1}/send_group_msg?group_id={2}&message={3}'.format(host,post,gid,msg))
-    else :
-        requests.get('http://{0}:{1}/send_private_msg?user_id={0}&message={1}'.format(host,post,uid,msg))
+        requests.get(
+            'http://{0}:{1}/send_group_msg?group_id={2}&message={3}'.format(host, post, gid, msg))
+    else:
+        requests.get(
+            'http://{0}:{1}/send_private_msg?user_id={0}&message={1}'.format(host, post, uid, msg))
 
-def poke(uid,gid = None):  #戳一戳
+
+def poke(uid, gid=None):  # 戳一戳
     '''戳一戳，uid为被戳的人，当gid为空时发送私聊戳一戳'''
-    msg='[CQ:poke,qq='+uid+']'
-    sendmsg(msg,uid,gid)
+    msg = '[CQ:poke,qq='+uid+']'
+    sendmsg(msg, uid, gid)
 # [CQ:poke,qq=<qq号>] 戳一戳的cq代码
 
-def songs(message,uid,gid = None):
+
+def songs(message, uid, gid=None):
     '''点歌，检测前二到五位是否是指定字符串，空一格后面数字是song id'''
     if message[:2] == 'QQ' or message[:2] == 'qq':
         song_id = message[3:]
         song_type = 'qq'
-    elif message[:4] == 'QQ音乐' or message[:4] == 'qq音乐' or message[:4] == 'qqyy' or message[:4] == 'QQ音樂'or message[:4] == 'qq音樂':
+    elif message[:4] == 'QQ音乐' or message[:4] == 'qq音乐' or message[:4] == 'qqyy' or message[:4] == 'QQ音樂' or message[:4] == 'qq音樂':
         song_id = message[5:]
         song_type = 'qq'
     elif message[:5] == '网易云音乐' or message[:5] == 'wyyyy' or message[:5] == '網易雲音樂':
@@ -49,6 +59,14 @@ def songs(message,uid,gid = None):
         song_id = message[3:]
         song_type = '163'
     msg = '[CQ:music,type='+song_type+',id='+song_id+']'
-    sendmsg(msg,uid,gid)
+    sendmsg(msg, uid, gid)
 
-## [CQ:music,type=源,id=音乐id]
+# [CQ:music,type=源,id=音乐id]
+
+
+def image(uid, gid = None, paths=None, url=None):
+    if paths != None:
+        msg = '[CQ:image,file=' + paths + ']'
+    else:
+        msg = '[CQ:image,url=' + url + ']'
+    sendmsg(msg,uid, gid )
